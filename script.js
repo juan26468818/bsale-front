a729aeb3521f5e89c331208e2dc56bec90a71845
juan <juanayala@gmail.com>
2022-11-11 02:33:48 -0300

    add discounts and access to shoping cart

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 const productContainer = document.getElementById("product-container");
 const categoriesContainer = document.getElementById("categories-container");
-
-
+const cartBg = document.getElementById("cart-bg");
+const cart = document.getElementById("cart")
+const openCart = document.getElementById("open-cart")
+const addToCart = document.getElementsByClassName("add-to-cart");
+let itemsInCart = 0;
 
 const displayProducts = (products)=>{
     let productsHTML = ""
@@ -16,14 +19,27 @@ const displayProducts = (products)=>{
                 <div class="product">
                     <p class="container__title">${element.name}</p>
                     <div class="product-description">
-                        <p class="container__price">$${element.price}</p>
-                        <i class="fa-solid fa-cart-shopping add-to-cart"></i>
+                    <p class="container__price">$${element.price}</p>
+                    <i class="fa-solid fa-cart-shopping add-to-cart" id=${element.id}></i>
                     </div>
-                </div>
-            </div>
-        `
-        productContainer.innerHTML = productsHTML
-    });
+                    <p style="color:red">${element.discount ? element.discount+"% de descuento" : ""}</p>
+                    </div>
+                    </div>
+                    `
+                    productContainer.innerHTML = productsHTML
+                    
+                });
+                for(let i = 0; i < products.length;i++){
+                    addToCart[i].addEventListener("click", (e)=>{
+                        itemsInCart += 1
+                        let item = localStorage.getItem(e.target.id)
+                        if(item === null){
+                            localStorage.setItem(e.target.id, 1)
+                        }else{
+                            localStorage.setItem(e.target.id, +item+1)
+                        }
+                    })
+                }
 }
 
 const displayCategories = (categories)=>{
@@ -40,7 +56,15 @@ const displayCategories = (categories)=>{
     });
 }
 
+cartBg.addEventListener("click", ()=>{
+    cartBg.classList += "hidden"
+    cart.classList += "hidden"
+})
 
+openCart.addEventListener("click", ()=>{
+    cartBg.classList.remove("hidden")
+    cart.classList.remove("hidden")
+})
 
 window.onload = async () =>{
 
