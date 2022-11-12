cafba9ad28e08c0a1a4d89bd0eb4fcb1936ecfdb
juan <juanayala@gmail.com>
2022-11-12 02:06:31 -0300

    add cart functionality and storage

--- script.js
+++ script.js
@@ -5,43 +5,67 @@ const cartBg = document.getElementById("cart-bg");
 const cart = document.getElementById("cart")
 const openCart = document.getElementById("open-cart")
 const addToCart = document.getElementsByClassName("add-to-cart");
-let itemsInCart = 0;
+const productOrder = document.getElementById("order-description");
+const totalBuyAmount = document.getElementById("total-buy__amount")
+const orderList = [];
+const cartItems = [];
+let checkDuplicate = [];
+let itemsInCart = +localStorage.getItem("Total")
+openCart.setAttribute("value", itemsInCart)
+for(let i = 0; i < localStorage.length; i++){
+    if(localStorage.key(i) !== "Total")
+        orderList.push(localStorage.key(i))
+        checkDuplicate.push(localStorage.key(i))
+}
+console.log(checkDuplicate)
+
 
 const displayProducts = (products)=>{
+
+    /* Añadir los HTML con los productos*/
     let productsHTML = ""
     products.forEach(element => {
         productsHTML+=
             `<div class="container">
                 <picture>
-                    
                     <img src=${element.url_image} class="container__img" alt=${element.name} onerror="this.src='./img/no-image-iconpng.png';" aria-hidden="true"/>
                 </picture>
                 <div class="product">
                     <p class="container__title">${element.name}</p>
                     <div class="product-description">
-                    <p class="container__price">$${element.price}</p>
-                    <i class="fa-solid fa-cart-shopping add-to-cart" id=${element.id}></i>
+                        <p class="container__price">$${element.price}</p>
+                        <i class="fa-solid fa-cart-shopping add-to-cart" id=${element.id}></i>
                     </div>
-                    <p style="color:red">${element.discount ? element.discount+"% de descuento" : ""}</p>
-                    </div>
-                    </div>
-                    `
-                    productContainer.innerHTML = productsHTML
-                    
-                });
-                for(let i = 0; i < products.length;i++){
-                    addToCart[i].addEventListener("click", (e)=>{
-                        itemsInCart += 1
-                        let item = localStorage.getItem(e.target.id)
-                        if(item === null){
-                            localStorage.setItem(e.target.id, 1)
-                        }else{
-                            localStorage.setItem(e.target.id, +item+1)
-                        }
-                    })
-                }
+                    <p class="product-discount">${element.discount ? element.discount+"% de descuento" : ""}</p>
+                </div>
+            </div>`
+        productContainer.innerHTML = productsHTML  
+    });
+
+    /* Sumatoria de la cantidad de productos agregados al carrito */
+    for(let i = 0; i < products.length;i++){
+        addToCart[i].addEventListener("click", async(e)=>{
+            orderList.push(e.target.id)
+            await getCartProducts()
+            itemsInCart += 1
+            let totalValue = localStorage.getItem("Total")
+
+            console.log(checkDuplicate)
+
+            localStorage.setItem("Total", +totalValue+1)
+            openCart.setAttribute("value", itemsInCart)
+            let item = localStorage.getItem(e.target.id)
+            if(item === null){
+                localStorage.setItem(e.target.id, 1)
+            }else{
+                localStorage.setItem(e.target.id, +item+1)
+            }
+        })
+    }
 }
 
+
+/* Mostrar las categrías */
 const displayCategories = (categories)=>{
     let categoriesHTML = ""
     categories.forEach(element => {
@@ -56,20 +80,74 @@ const displayCategories = (categories)=>{
     });
 }
 
+
+/* Debo seleccionar el keys del local storage y si este es igual a el id de x producto debo agregarlo */
+
+const displayCart =  ((products) =>{
+    
+    /* Mostrar los productos en la zona del carrito de compras */
+    /* Necesito que los productos que ingresen sean los productos que se encuentran en el localStorage */
+    let productsHTML = ""
+    let total = 0
+    products.forEach(element => {
+        total += (+element.price*+element.quantity) - (+element.discount*+element.quantity)
+        productsHTML+=
+        `
+        <ul class="order-container">
+            <li class="order-product"><span class="product-quantity">${element.quantity}</span>${element.name} <span class="product-price">$${element.price}</span></li>
+            <li class="product-price">$${element.discount}</li>
+        </ul>`;
+        
+        productOrder.innerHTML = productsHTML;
+    });
+    totalBuyAmount.innerHTML = total
+    console.log(products)
+})
+
+
+
+/* Abrir Carrito de compras */
+openCart.addEventListener("click", async()=>{
+    cartBg.classList.remove("hidden")
+    cart.classList.remove("hidden")
+    displayCart(cartItems)
+})
+
+
+/* Cerrar Carrito de compras */
 cartBg.addEventListener("click", ()=>{
     cartBg.classList += "hidden"
     cart.classList += "hidden"
 })
+//Obtiene los productos del carrito que se encontraban guardados en el localStorage
+const getFirstCartProducts = (async()=>{
+    for(let i = 0; i < orderList.length; i++){
+        const cartProducts = await(await fetch(`/api/products/${orderList[i]}`)).json();
+        cartItems.push(cartProducts)
+        cartItems[i].quantity = localStorage.getItem(orderList[i])
+    }
+})
 
-openCart.addEventListener("click", ()=>{
-    cartBg.classList.remove("hidden")
-    cart.classList.remove("hidden")
+// Actualiza los productos en el carrito
+const getCartProducts = (async()=>{
+    for(let i = orderList.length-1; i < orderList.length; i++){
+        const cartProducts = await(await fetch(`/api/products/${orderList[i]}`)).json();
+        let test = cartProducts.id
+        console.log(checkDuplicate.includes((test).toString()))
+        checkDuplicate.push((test).toString())
+        // if(!orderList.includes((i).toString())){
+        //     // cartProducts[orderList.indexOf((i).toString())].quantity = "test"
+        // }else{
+        //     cartItems.push(cartProducts)
+        //     cartItems[i].quantity = localStorage.getItem(orderList[i])
+        // }
+    }
 })
 
 window.onload = async () =>{
-
     const allProducts = await(await fetch("/api/products")).json(); 
-    const allCategories = await(await fetch("/api/categories")).json(); 
+    const allCategories = await(await fetch("/api/categories")).json();
     displayProducts(allProducts)
     displayCategories(allCategories)
+    getFirstCartProducts()
 }
