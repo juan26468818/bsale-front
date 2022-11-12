b34bb092376bb0c89e8a3d9abdedd298ada8068f
juan <juanayala@gmail.com>
2022-11-12 18:24:35 -0300

    finish filter options

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const productContainer = document.getElementById("product-container");
 const categoriesContainer = document.getElementById("categories-container");
 const cartBg = document.getElementById("cart-bg");
@@ -7,17 +6,29 @@ const openCart = document.getElementById("open-cart")
 const addToCart = document.getElementsByClassName("add-to-cart");
 const productOrder = document.getElementById("order-description");
 const totalBuyAmount = document.getElementById("total-buy__amount")
+const filterBtn = document.getElementById("filter")
+let filterList = 1
+const categoriesForm = document.forms['category-form']
 const orderList = [];
 const cartItems = [];
 let checkDuplicate = [];
 let itemsInCart = +localStorage.getItem("Total")
 openCart.setAttribute("value", itemsInCart)
+
+
+/* DisplayProducts Filtered */
+filterBtn.addEventListener("click", async(e)=>{
+    e.preventDefault()
+    filterList = categoriesForm.categories.value;
+    const productsByCategory = await(await fetch(`/api/products/category/${filterList}`)).json();
+    displayProducts(productsByCategory)
+})
+
 for(let i = 0; i < localStorage.length; i++){
     if(localStorage.key(i) !== "Total")
         orderList.push(localStorage.key(i))
         checkDuplicate.push(localStorage.key(i))
 }
-console.log(checkDuplicate)
 
 
 const displayProducts = (products)=>{
@@ -50,7 +61,6 @@ const displayProducts = (products)=>{
             itemsInCart += 1
             let totalValue = localStorage.getItem("Total")
 
-            console.log(checkDuplicate)
 
             localStorage.setItem("Total", +totalValue+1)
             openCart.setAttribute("value", itemsInCart)
@@ -71,10 +81,9 @@ const displayCategories = (categories)=>{
     categories.forEach(element => {
         categoriesHTML+=
             `
-                <label class="categories">
-                    <input type="checkbox" class="category-check">
+                <option class="categories" value=${element.id}>
                     ${element.name.charAt(0).toUpperCase() + element.name.slice(1)}
-                </label>
+                </option>
         `
         categoriesContainer.innerHTML = categoriesHTML
     });
@@ -101,7 +110,6 @@ const displayCart =  ((products) =>{
         productOrder.innerHTML = productsHTML;
     });
     totalBuyAmount.innerHTML = total
-    console.log(products)
 })
 
 
@@ -119,7 +127,7 @@ cartBg.addEventListener("click", ()=>{
     cartBg.classList += "hidden"
     cart.classList += "hidden"
 })
-//Obtiene los productos del carrito que se encontraban guardados en el localStorage
+// Obtiene los productos del carrito que se encontraban guardados en el localStorage
 const getFirstCartProducts = (async()=>{
     for(let i = 0; i < orderList.length; i++){
         const cartProducts = await(await fetch(`/api/products/${orderList[i]}`)).json();
@@ -127,26 +135,64 @@ const getFirstCartProducts = (async()=>{
         cartItems[i].quantity = localStorage.getItem(orderList[i])
     }
 })
+// Compara dos Objetos
+function compareObj(a, b) {
+    let aKeys = Object.keys(a).sort();
+    let bKeys = Object.keys(b).sort();
+    if (aKeys.length !== bKeys.length) {
+        return false;
+    }
+    if (aKeys.join('') !== bKeys.join('')) {
+        return false;
+    }
+    for (let i = 0; i < aKeys.length; i++) {
+        if ( a[aKeys[i]]  !== b[bKeys[i]]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 // Actualiza los productos en el carrito
 const getCartProducts = (async()=>{
     for(let i = orderList.length-1; i < orderList.length; i++){
         const cartProducts = await(await fetch(`/api/products/${orderList[i]}`)).json();
-        let test = cartProducts.id
-        console.log(checkDuplicate.includes((test).toString()))
-        checkDuplicate.push((test).toString())
-        // if(!orderList.includes((i).toString())){
-        //     // cartProducts[orderList.indexOf((i).toString())].quantity = "test"
-        // }else{
-        //     cartItems.push(cartProducts)
-        //     cartItems[i].quantity = localStorage.getItem(orderList[i])
-        // }
+        let cartPID = cartProducts.id
+        
+
+        if(checkDuplicate.includes((cartPID).toString())){
+            for(let i = 0; i < cartItems.length; i++){
+                cartProducts.quantity = cartItems[i].quantity
+                if(compareObj(cartProducts, cartItems[i])){
+                    cartItems[i].quantity = parseInt(cartItems[i].quantity) + 1
+                }
+            }
+        }else{
+            cartProducts.quantity = +1
+            cartItems.push(cartProducts)
+        }
+
+        checkDuplicate.push((cartPID).toString())
+
+        
     }
 })
+
+
 
 window.onload = async () =>{
     const allProducts = await(await fetch("/api/products")).json(); 
     const allCategories = await(await fetch("/api/categories")).json();
+    allProducts.sort(function (a, b) {
+        if (a.category > b.category) {
+          return 1;
+        }
+        if (a.category < b.category) {
+          return -1;
+        }
+        // a must be equal to b
+        return 0;
+      });
     displayProducts(allProducts)
     displayCategories(allCategories)
     getFirstCartProducts()
